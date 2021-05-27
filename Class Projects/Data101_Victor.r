dbe1eff811d55a6a2bdd6c5a17d7a75a8b32f550
Víctor Espinoza <victor.espinozaj@outlook.com>
2021-05-27 16:08:35 -0500

    Create Data101_Victor.r
    Agregué un comentario nuevo en la linea 4

---
+++ Class Projects/Data101_Victor.r
@@ -0,0 +1,117 @@
+#Downloading data from different repositories
+# we will first collect and explore the data available at https://www.climatewatchdata.org
+
+#Esta ya es mi versi�n del archivo Data101
+
+#install the required libraries
+install.packages(c("httr", "jsonlite"))
+
+#load the libraries
+library(httr)
+library(jsonlite)
+
+#NDC
+ url <- "https://www.climatewatchdata.org/api/v1/data/ndc_content"
+
+#make a get request
+
+ resNDC <- GET(url)
+
+#transform this to actual data
+
+ NDCdata<-fromJSON(rawToChar(resNDC$content))
+
+#NDCdata is list, let's explore its structure
+
+ str(NDCdata)
+
+#so we are getting a data frame with only 50 lines, this is obviously incomplete,
+#let's learn more about this by exploring the structure of resNDC
+ str(resNDC)
+
+#we should also explore what the GET function is doing, try the following
+
+?GET
+
+# in this website we can learn about what parameters we can use to control the data request
+#https://www.climatewatchdata.org/data-explorer/ndc-content?ndc-content-categories=unfccc_process&ndc-content-countries=All%20Selected&ndc-content-indicators=All%20Selected&ndc-content-sectors=All%20Selected&page=1
+#So it looks like we can refine this, let's try the process again, but now using the countries field to access the data of the European Union
+ resNDC<-GET(url,query= list(countries = "EUU", page=1))
+ NDCdata<-fromJSON(rawToChar(resNDC$content))
+
+#what sort of object NDCdata is ?
+ class(NDCdata)
+
+#what is the structure
+ str(NDCdata)
+
+#it looks we are only getting 50 observations per hit, let's look at the structure of resNDC again
+ str(resNDC)
+
+#so under headers, the field total says there 437 rows associated with this country, and the per page header says we are getting 50 rows per packages
+#we will need a loop to get all this data, let's try the following
+#first, let's set the parameters for the loop
+ total<-as.numeric(resNDC$headers$total)
+ per.page<-as.numeric(resNDC$headers['per-page'])
+ pages<-ceiling(total/per.page)
+
+#one way of doing this is using the for function
+#intialize the loop
+ resNDC<-GET(url,query= list(countries = "EUU", page=1))
+ NDCdata<-fromJSON(rawToChar(resNDC$content))$data
+for(i in 2:pages)
+{
+ resNDC<-GET(url,query= list(countries = "EUU", page=i))
+ pivot<-fromJSON(rawToChar(resNDC$content))$data
+ NDCdata<-rbind(NDCdata,pivot)
+}
+
+#check the dimmensions of this new object and see if the loop worked
+ dim(NDCdata)
+
+#it looks like it worked, but maybe we use too many lines to do this, we can do the same process with fewer lines using lapply
+NDCdata.best<-lapply(c(1:pages),function(x){
+                            resNDC<-GET(url,query= list(countries = "EUU", page=x));
+                             fromJSON(rawToChar(resNDC$content))$data
+                             }
+                      )
+
+NDCdata.best<-do.call(rbind,NDCdata.best)
+# you can see we get the same object
+dim(NDCdata.best)
+
+#Next we need to do this for all countries and all available data in this repository
+#let's explore the database
+ colnames(NDCdata)
+ names(NDCdata)
+ summary(NDCdata)
+#get unique values of variable
+ unique(NDCdata$iso_code3)
+#let's do all at once
+ sapply(NDCdata,unique)
+ sapply(NDCdata,max)
+
+#let's do this for all countries
+
+#primero tengo que obtenter un vector con las claves de los países
+#segundo tengo que estimar cuantas páginas por país hay
+#tercero en función de eso programar mi lapply para que haga rbind en todas las tables }
+# guardar un csv de cada pais en mi carpeta
+# este proceso tiene que ocurrir en todos los paises
+
+
+#segunda opción jalo la base completa, y el loop corre 2003 .
+#guardo el archivo
+
+
+NamesC<-
+for (i in 1:length(NamesC))
+{
+NDCdata.best<-lapply(c(1:pages),function(x){
+                            resNDC<-GET(url,query= list(countries = NamesC[i] , page=x));
+                             fromJSON(rawToChar(resNDC$content))$data
+                             }
+                      )
+NDCdata.best<-do.call(rbind,NDCdata.best)
+
+}
